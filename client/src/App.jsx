aacd28d9121d55e9c50443d3b5910785fe4e003f
parvezalam4114 <parvezalam4114s@gmail.com>
2026-07-23 22:06:11 +0530

    Initial React Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>CareConnect</h1>
+      <p>Learning React with Pasa 🚀</p>
+    </div>
+  );
+}
+
+export default App;
